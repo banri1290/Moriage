bb2b42a800bdfb0e7330123a070b2ea8fbb07b5e
banri1290 <banri_129@icloud.com>
2025-12-05 16:11:16 +0900

    タイトル修正

--- Co-Can/Assets/Title/TitleManager.cs
+++ Co-Can/Assets/Title/TitleManager.cs
@@ -6,6 +6,6 @@ public class TitleManager : MonoBehaviour
     public void OnStartButtonPressed()
     {
         // ゲーム本編のシーンに切り替え
-        SceneManager.LoadScene("CopyScene20251024");
+        SceneManager.LoadScene("MainScene");
     }
 }
